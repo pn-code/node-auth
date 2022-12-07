4d721659b19862c971e5e78ac39ad5b36e93f06e
Philip Nguyen <philipnguyen953@gmail.com>
2022-12-06 21:02:16 -0800

    Added and secured mongo_url

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
+const dotEnv = require('dotenv').config()
+
 const Schema = mongoose.Schema;
 
-const mongoDb = "YOUR MONGO URL HERE";
+const mongoDb = process.env.MONGO_URL
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -28,6 +30,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+    res.render("index")
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
